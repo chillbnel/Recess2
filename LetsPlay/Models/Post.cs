2c7629dffa02c406e4fedd493d225f96c3d5685b
Richard J <richjim89@gmail.com>
2018-12-10 13:32:49 -0800

    Post model

---
+++ LetsPlay/Models/Post.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models
+{
+    public class Post
+    {
+        public int ID { get; set; } //Primary Key, auto - by DB
+        public string Username { get; set; } //auto
+        [Required]
+        public string Title { get; set; }
+        public DateTime PostedDate { get; set; } //auto
+        [Required]
+        [EnumDataType(typeof(EventType))]
+        public EventType Type { get; set; } // dropdown
+        [Required]
+        [EnumDataType(typeof(Category))]
+        public Category Category { get; set; } //dropdown
+        [Required]
+        public string Game { get; set; }
+        public string Description { get; set; }
+        public DateTime EventDate { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public string Location { get; set; }
+
+        public PlayerSignups Signups { get; set; }
+    }
+
+    public enum EventType
+    {
+        Event,
+        LFM
+    }
+
+    public enum Category
+    {
+        Sports,
+        VideoGames,
+        Tabletop,
+        Misc
+    }
+}
