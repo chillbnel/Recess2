9b6d06b87dca33ccc0979fb64ac0aeeaa3ac0fb0
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-12-10 15:37:25 -0800

    ApplicationUser model created

---
+++ LetsPlay/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string firstName { get; set; }
+
+        [Required]
+        [Display(Name = "Gamer Tag")]
+        public string gamerTag { get; set; }
+
+        [Required]
+        [Display(Name = "Age")]
+        public DateTime age { get; set; }
+
+        [Required]
+        [Display(Name = "Your Location")]
+        public DateTime location { get; set; }
+    }
+}
