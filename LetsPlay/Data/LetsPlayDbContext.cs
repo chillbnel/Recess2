83ec1e34757ed575c325c02d47d25794f582cab5
Richard J <richjim89@gmail.com>
2018-12-10 14:12:08 -0800

    Created database

--- LetsPlay/Data/LetsPlayDbContext.cs
+++ LetsPlay/Data/LetsPlayDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LetsPlay.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,21 @@ namespace LetsPlay.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Friendships>().HasKey(
+                ce => new { ce.User1, ce.User2 }
+                );
+
+            modelBuilder.Entity<PlayerSignups>().HasKey(
+                ce => new { ce.Username, ce.PostID }
+                );
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<Friendships> Friendships { get; set; }
+        public DbSet<Messages> Messages { get; set; }
+        public DbSet<PlayerSignups> Signups { get; set; }
     }
 }
