3d9048ca0f8895ac79ecb8328ecb0df17beb2a41
Richard J <richjim89@gmail.com>
2018-12-10 13:08:07 -0800

    DB Context

---
+++ LetsPlay/Data/LetsPlayDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Data
+{
+    public class LetsPlayDbContext : DbContext
+    {
+        public LetsPlayDbContext(DbContextOptions<LetsPlayDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
