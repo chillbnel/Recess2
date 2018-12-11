a73747de3ba7b7bcfc60841f7536bb3a9a6fcf7c
Richard J <richjim89@gmail.com>
2018-12-10 16:01:25 -0800

    fixed sidenav

--- LetsPlay/wwwroot/JavaScript.js
+++ LetsPlay/wwwroot/JavaScript.js
@@ -1,7 +1,10 @@
-﻿$(document).ready(function () {
+﻿/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+}
 
-});
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
