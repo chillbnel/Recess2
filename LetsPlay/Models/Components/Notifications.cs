a595f0c37e6e8ce4a55218cdbdaaf6d662a824a5
Richard J <richjim89@gmail.com>
2018-12-11 14:53:27 -0800

    messages/notifications component

---
+++ LetsPlay/Models/Components/Notifications.cs
@@ -0,0 +1,21 @@
+﻿using LetsPlay.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models.Components
+{
+    public class Notifications : ViewComponent
+    {
+        private IMessages _messages;
+
+        public Notifications(IMessages messages)
+        {
+            _messages = messages;
+        }
+
+
+    }
+}
