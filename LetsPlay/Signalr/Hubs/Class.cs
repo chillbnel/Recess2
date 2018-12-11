76e800373e2458c354b5b00828c997e0d88840f9
Jeff Weng <jhwe92@gmail.com>
2018-12-11 14:11:59 -0800

    create hub class

---
+++ LetsPlay/Signalr/Hubs/Class.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Signalr.Hubs
+{
+    public class Chathub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.SendAsync(name, message);
+        }
+    }
+}
