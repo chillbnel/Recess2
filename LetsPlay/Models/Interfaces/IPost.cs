18e0075ad1a17bb21632730b34773059452ce0e0
carlos cadena <carloscadena@live.com>
2018-12-11 10:53:53 -0800

    IPost interface implementation

---
+++ LetsPlay/Models/Interfaces/IPost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models.Interfaces
+{
+    public interface IPost
+    {
+        Task<IEnumerable<Post>> GetAllPosts();
+
+        Task<Post> GetPost(int? id);
+
+        Task CreatePost(Post post);
+
+        Task UpdatePost(Post post);
+
+        Task DeletePost(int id);
+    }
+}
