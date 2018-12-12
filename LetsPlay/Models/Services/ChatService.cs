0fc72581aadad244e74be25934cbfc730acd877c
Jeff Weng <jhwe92@gmail.com>
2018-12-12 12:35:26 -0800

    added chatservice

---
+++ LetsPlay/Models/Services/ChatService.cs
@@ -0,0 +1,18 @@
+﻿using LetsPlay.Data;
+using LetsPlay.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models.Services
+{
+    public class ChatService : IChat
+    {
+        private LetsPlayDbContext _context;
+        public Task<IEnumerable<GeneralChat>> GetMessages()
+        {
+            
+        }
+    }
+}
