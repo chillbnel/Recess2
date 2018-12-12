7ed1917d844f320df8a446c0cd6f1e6f496db58d
Jeff Weng <jhwe92@gmail.com>
2018-12-12 11:41:46 -0800

    added general chat model

---
+++ LetsPlay/Models/GeneralChat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models
+{
+    public class GeneralChat
+    {
+        public int ID { get; set; }
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
