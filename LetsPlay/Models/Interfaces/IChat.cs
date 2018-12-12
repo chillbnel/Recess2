fb14aaccdb7ab2f0a95f0ab1c248cdc458f2674e
Jeff Weng <jhwe92@gmail.com>
2018-12-12 12:28:27 -0800

    added IChat interface

---
+++ LetsPlay/Models/Interfaces/IChat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models.Interfaces
+{
+    interface IChat
+    {
+        Task<IEnumerable<GeneralChat>> GetMessages();
+    }
+}
