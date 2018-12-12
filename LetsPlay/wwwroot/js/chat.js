067d91ae337d89f9b40fa56ea222381d9d244785
Jeff Weng <jhwe92@gmail.com>
2018-12-11 16:09:17 -0800

    comments for chat.js

--- LetsPlay/wwwroot/js/chat.js
+++ LetsPlay/wwwroot/js/chat.js
@@ -1,23 +1,27 @@
-﻿"use strict";
+﻿'use strict';
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+//Set up connection
+var connection = new signalR.HubConnectionBuilder().withUrl('/chatHub').build();
 
+// Parses message and appends to ul
 connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    var encodedMsg = user + ' says ' + msg;
+    var li = document.createElement('li');
     li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    document.getElementById('messagesList').appendChild(li);
 });
 
+// Error handling for connection
 connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+// Logic message sending button
+document.getElementById('sendButton').addEventListener('click', function (event) {
+    var user = document.getElementById('userInput').value;
+    var message = document.getElementById('messageInput').value;
+    connection.invoke('SendMessage', user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
