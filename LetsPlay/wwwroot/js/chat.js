e3939194bb967c20eb1d97cd215631b0a53dd25f
Jeff Weng <jhwe92@gmail.com>
2018-12-12 15:43:29 -0800

    user now must be logged in to send message. message is visible to everyone

--- LetsPlay/wwwroot/js/chat.js
+++ LetsPlay/wwwroot/js/chat.js
@@ -6,7 +6,7 @@ var connection = new signalR.HubConnectionBuilder().withUrl('/chatHub').build();
 // Parses message and appends to ul
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    var encodedMsg = user + ' says ' + msg;
+    var encodedMsg = user + ': ' + msg;
     var li = document.createElement('li');
     li.textContent = encodedMsg;
     document.getElementById('messagesList').appendChild(li);
