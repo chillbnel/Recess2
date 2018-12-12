4529023ca04bc1ae94cbe5b88b867c58784dbd78
Richard J <richjim89@gmail.com>
2018-12-11 16:13:50 -0800

    interface

---
+++ LetsPlay/Models/Interfaces/IFriendships.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models.Interfaces
+{
+    public interface IFriendships
+    {
+        Task<IEnumerable<Friendships>> GetAllFriendships();
+
+        Task<IEnumerable<Friendships>> GetFriendshipsForUser();
+
+        Task CreateFriendRequest();
+
+        Task AcceptFriendRequest();
+
+        Task RemoveFriend();
+    }
+}
