8298564f0a9ee063523ca642fa31f2f75b280ac7
Richard J <richjim89@gmail.com>
2018-12-13 18:20:20 -0800

    friends list

---
+++ LetsPlay/Models/Components/FriendList.cs
@@ -0,0 +1,26 @@
+﻿using LetsPlay.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsPlay.Models.Components
+{
+    public class FriendList : ViewComponent
+    {
+        private IFriendships _friendships;
+
+        public FriendList(IFriendships friendships)
+        {
+            _friendships = friendships;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string username)
+        {
+            var friends = await _friendships.GetFriendshipsForUser(username);
+
+            return View(friends);
+        }
+    }
+}
