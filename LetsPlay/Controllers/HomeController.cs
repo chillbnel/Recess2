24e03867121ab6a76e6b716a488848282d9311da
Jeff Weng <jhwe92@gmail.com>
2018-12-13 17:21:18 -0800

    init migration chatroom

--- LetsPlay/Controllers/HomeController.cs
+++ LetsPlay/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using LetsPlay.Models;
 using LetsPlay.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LetsPlay.Controllers
@@ -12,17 +13,36 @@ namespace LetsPlay.Controllers
     {
         private readonly IChat _chat;
         private readonly IPost _post;
+        private readonly IFriendships _frendships;
+        private UserManager<ApplicationUser> _userManager { get; set; }
+        private SignInManager<ApplicationUser> _signInManager;
+        public ApplicationUser CurrentUser { get; set; }
 
-        public HomeController(IChat chat, IPost post)
+        public HomeController(IChat chat, IPost post, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IFriendships friendships)
         {
             _chat = chat;
             _post = post;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _frendships = friendships;
+            
         }
 
         public async Task<IActionResult> Index()
         {
-            ViewBag.GeneralChat = await _chat.GetMessages();
+            if (_signInManager.IsSignedIn(User))
+            {
+                CurrentUser = await _userManager.GetUserAsync(HttpContext.User);
+                ViewBag.Friends = await _frendships.GetFriendshipsForUser(CurrentUser.UserName);
+            }
+            
             ViewBag.Posts = await _post.GetLastTenPosts();
+            return View();
+        }
+
+        public async Task<IActionResult> ChatRoom()
+        {
+            ViewBag.GeneralChat = await _chat.GetMessages();
             return View();
         }
     }
